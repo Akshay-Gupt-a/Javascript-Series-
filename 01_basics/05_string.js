0083a9bd58da16bb785d96d734f0a4a8132e69c2
Akshay Gupta <99547720+Akshay-Gupt-a@users.noreply.github.com>
2025-02-20 14:05:52 +0000

    String  Creating

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+// "" '' ``
+let name="Akshay"
+console.log(typeof name)//string
+let repoCount=100
+console.log(name+repoCount+" value")//Akshay100 value
+// It is not good way
+console.log(`Hello ${name} you code value is ${repoCount}`)//Hello Akshay you code value is 100
+
+const gameName=new String("Akshay")// Now it will use as Object ( it is string but now behave as object You wiil get so much method of it)
