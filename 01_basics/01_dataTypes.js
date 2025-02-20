57362188198f9caa7b8f96213d8db52fc2d151fc
Akshay Gupta <99547720+Akshay-Gupt-a@users.noreply.github.com>
2025-02-20 10:43:51 +0000

    dataTypes discussion

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS codes as newer version
+// alert(3+3) // we are using nodejs , not browser
+//console.log(3+3); console.log("Akshay");// Not good habbit , code should be readable
+
+// number => 2 to power 53
+let age=18
+// string '' "" ``
+// boolean => true/false
+// null => standalone value // Consider you are fetching weather but it's unable to send then it's send null (not 0 will send not undefined it will send null means nothing )
+// undefined => not give value;
+// symbol =>unique
+
+//object
+
+console.log(typeof("2"))//string
+console.log(typeof null)//object (language error)
+console.log(typeof undefined)// undefined
